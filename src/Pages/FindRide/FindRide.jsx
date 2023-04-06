6859924d2cd0f4c9821c282a8123b6e750863dff
Artlima1 <arthurdeolima@gmail.com>
2023-04-06 12:13:09 -0300

    Basic DB use in pages offer ride & find ride

--- src/Pages/FindRide/FindRide.jsx
+++ src/Pages/FindRide/FindRide.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import {getAllRides, getWithFilters } from '../../db/db'
+import { useEffect, useState } from "react";
 
 const FindRide = () => {
-  return <h1>FindRide</h1>;
+  const [rides, setRides] = useState([]);
+
+  const fetchRides = async ()=> {
+    try{
+      const currRides = await getAllRides();
+      console.log(currRides);
+      setRides([...currRides]);
+    }
+    catch(e){
+      console.error(e);
+    }
+  }
+
+  const fetchFilteredRides = async ()=> {
+    try{
+      const currRides = await getWithFilters([
+        {
+          key: "to",
+          op: "==",
+          value: "UFMG"
+        },
+        {
+          key: "from",
+          op: "==",
+          value: "Olegario"
+        },
+        {
+          key: "time", 
+          op: ">=",
+          value: (new Date(2023, 3, 10))
+        },
+      ]);
+      console.log(currRides);
+      setRides(currRides);
+    }
+    catch(e){
+      console.error(e);
+    }
+  }
+
+  useEffect(()=>{
+    fetchFilteredRides();
+  },[])
+
+  return (
+    <div>
+      {
+        rides.map( ride => {
+          return(
+            <li>
+              {ride.from} - {ride.to} ({ride.time.toUTCString()})
+            </li>
+          )
+        })
+      }
+    </div>
+  );
 };
 
 export default FindRide;
--- src/Pages/OfferRide/OfferRide.jsx
+++ src/Pages/OfferRide/OfferRide.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button } from "antd";
+import {createRide } from '../../db/db'
 
 const OfferRide = () => {
-  return <h1>OfferRide</h1>;
+
+  const createNew = async (e) => {
+    e.preventDefault();
+
+    const ride = {
+      owner: {
+        name: "Olegario_UFMG",
+        phone: "44815",
+      },
+      from: "Olegario",
+      to: "UFMG",
+      time: new Date(),
+      seats: 3,
+    }
+
+    try{
+      const id = await createRide(ride);
+      console.log(id);
+    }
+    catch(e) {
+      console.error(e);
+    };
+
+  }
+
+  return (
+    <div>
+      <Button type="primary" block onClick={createNew}>
+        CRIAR
+      </Button>
+    </div>
+  );
 };
 
 export default OfferRide;
