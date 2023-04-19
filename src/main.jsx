1dde5d003e5238231647286e211961e6ad986675
Artlima1 <arthurdeolima@gmail.com>
2023-04-19 16:43:20 -0300

    User context and login fully functional

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,11 @@
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-import Layout from "./Components/Layout/Layout";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import FindRide from "./Pages/FindRide/FindRide";
-import OfferRide from "./Pages/OfferRide/OfferRide";
-import RideInfo from "./Pages/RideInfo/RideInfo";
-
-import "./index.css"
-
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Login />} />
-        <Route path="home" element={<Home />} />
-        <Route path="buscar" element={<FindRide />} />
-        <Route path="oferecer" element={<OfferRide />} />
-        <Route path="informacoes" element={<RideInfo />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
