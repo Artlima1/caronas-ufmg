1349e7f626d0cee9b5e28d6266b75e7638ba0105
Clara Martins <cmcm11320@gmail.com>
2023-05-08 13:55:15 -0300

    Filter rides

--- src/Pages/FindRide/FindRide.jsx
+++ src/Pages/FindRide/FindRide.jsx
@@ -3,14 +3,16 @@ import {getAllRides, getWithFilters } from '../../utils/db'
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
+
 const FindRide = () => {
   const [rides, setRides] = useState([]);
+  const [filters, setFilters] = useState({});
   const navigate = useNavigate();
 
   const fetchRides = async ()=> {
     try{
       const currRides = await getAllRides();
-      console.log(currRides);
       setRides([...currRides]);
     }
     catch(e){
@@ -21,21 +23,21 @@ const FindRide = () => {
   const fetchFilteredRides = async ()=> {
     try{
       const currRides = await getWithFilters([
-        // {
-        //   key: "to",
-        //   op: "==",
-        //   value: "UFMG"
-        // },
-        // {
-        //   key: "from",
-        //   op: "==",
-        //   value: "Olegario"
-        // },
-        // {
-        //   key: "time", 
-        //   op: ">=",
-        //   value: (new Date(2023, 3, 10))
-        // },
+        {
+          key: "to",
+          op: "==",
+          value: filters.to || "",
+        },
+        {
+          key: "from",
+          op: "==",
+          value: filters.from || "",
+        },
+        {
+          key: "time", 
+          op: ">=",
+          value: filters.time || new Date(),
+        },
       ]);
       setRides(currRides);
     }
@@ -44,12 +46,31 @@ const FindRide = () => {
     }
   }
 
-  useEffect(()=>{
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilters({ ...filters, [name]: value });
+  };
+
+  const handleFilterSubmit = (event) => {
+    event.preventDefault();
     fetchFilteredRides();
+  };
+
+  useEffect(()=>{
+    fetchRides();
   },[])
 
   return (
     <div>
+      <form onSubmit={handleFilterSubmit}>
+        {/* <input type="text" name="Partida" onChange={handleFilterChange} placeholder="Partida" style="padding: 1%;border-radius: 10px 0px 0px 10px;" />
+        <input type="text" name="Destino" onChange={handleFilterChange} placeholder="Destino" style="padding: 1%;"/>
+        <input type="datetime-local" name="time" onChange={handleFilterChange} style="padding: 1%;border-radius: 0px 10px 10px 0px;"/> */}
+        <input type="text" name="to" onChange={handleFilterChange} placeholder="Partida" />
+        <input type="text" name="from" onChange={handleFilterChange} placeholder="Destino"/>
+        <input type="datetime-local" name="time" onChange={handleFilterChange} />
+        <button type="submit">Filtrar</button>
+      </form>
       {
         rides.map( ride => {
           return(
